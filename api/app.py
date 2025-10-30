787a3fa69b29aa5f35a766af4e038811496c0c36
404unkown <francomanucho9@gmail.com>
2025-10-30 11:47:17 +0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,8 +1,4 @@
 from web import create_app
 
-# Create Flask app
+# Vercel expects "app" as the entrypoint
 app = create_app()
-
-# Serverless entrypoint
-def handler(request, response):
-    return app(request.environ, response.start_response)
